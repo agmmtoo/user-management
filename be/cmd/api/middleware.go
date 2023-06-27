80119b76029d06161e427509b499fb445685561e
ammo <solidifyarmor@gmail.com>
2023-06-27 22:41:42 +0630

    auth middleware

--- be/cmd/api/middleware.go
+++ be/cmd/api/middleware.go
@@ -1,9 +1,21 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"strings"
+)
 
 func (app *application) authenticate(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, *http.Request) {
-		token := 
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+
+		headerParts := strings.Split(authHeader, " ")
+		if len(headerParts) != 2 || headerParts[0] != "Bearer" {
+			app.invalidCredentialsResponse(w, r)
+			return
+		}
+		// token := headerParts[1]
+
+		next.ServeHTTP(w, r)
 	})
 }
